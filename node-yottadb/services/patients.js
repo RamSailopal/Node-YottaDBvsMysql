15536aa65502d494a7f9143a417785179baec584
Raman Sailopal <rsailopal@robinsonbrothers.co.uk>
2022-10-07 16:03:43 +0100

    Amendments to get interation with YottaDB working

--- node-yottadb/services/patients.js
+++ node-yottadb/services/patients.js
@@ -7,33 +7,34 @@ async function getMultiple(page = 1) {
   var sex=ydb.get({global: 'PATIENTS', subscripts: [pat.result, 'sex']});
   var age=ydb.get({global: 'PATIENTS', subscripts: [pat.result, 'age']});
   var name=ydb.get({global: 'PATIENTS', subscripts: [pat.result, 'name']});
-  var address=ydb.get({global: 'PATIENTS', subscripts: [pat.result, 'address']});
-  jsonstr.push='({ "id": "' + id + '", "name": "' + name.data + '", "age: "' + age.data + '", "sex":  "' + sex.data + '", "address: "' + address.data + '" })'
+  jsonstr.push({ id: id, name: name.data, age: age.data, sex: sex.data })
   pat=ydb.order({global: 'PATIENTS', subscripts: [pat.result]});
-  var jsonstr1=JSON.stringify(jsonstr);
-  return jsonstr1;
+  return jsonstr;
 }
 
 async function create(patients) {
   var pat=ydb.previous({global: 'PATIENTS', subscripts: ['']});
+  if (isNaN(pat)) {
+    pat=0;
+  }
   let id=parseInt(pat)+1;
-  ydb.set('^PATIENTS', id, "name", "test")
-  ydb.set('^PATIENTS', id, "age", "42")
-  ydb.set('^PATIENTS', id, "age", "M")
+  ydb.set('^PATIENTS', parseInt(id), "name", patients.name)
+  ydb.set('^PATIENTS', parseInt(id), "age", patients.age)
+  ydb.set('^PATIENTS', parseInt(id), "sex", patients.sex)
   let message = "Patient created successfully";
   return { message };
 }
 
 async function update(id, patients) {
-  ydb.set('^PATIENTS', id, "name", patients.name)
-  ydb.set('^PATIENTS', id, "age", patients.age)
-  ydb.set('^PATIENTS', id, "age", patients.sex)
+  ydb.set('^PATIENTS', parseInt(id), "name", patients.name)
+  ydb.set('^PATIENTS', parseInt(id), "age", patients.age)
+  ydb.set('^PATIENTS', parseInt(id), "age", patients.sex)
   let message = "Patient updated successfully";
   return { message };
 }
 
 async function remove(id) {
-  ydb.kill('^PATIENTS', id)
+  ydb.kill('^PATIENTS', parseInt(id))
   let message = "Patient deleted successfully";
   return { message };
 }
